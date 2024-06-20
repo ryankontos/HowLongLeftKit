76b297ca6451b91c04308f8504ba2ab35dc5512a
Ryan Kontos <ryanjkontos@gmail.com>
2024-06-21 01:18:39 +1000

    No longer dynamic

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "HowLongLeftKit",
-            type: .dynamic,
             targets: ["HowLongLeftKit"]),
     ],
     dependencies: [
