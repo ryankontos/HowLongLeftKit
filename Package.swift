7dd6781b8bc54860ec678c47c7436a59f4f1aa38
Ryan Kontos <ryanjkontos@gmail.com>
2024-06-21 22:23:52 +1000

    Reverted nightmare

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "HowLongLeftKit",
+            type: .dynamic,
             targets: ["HowLongLeftKit"]),
     ],
     dependencies: [
